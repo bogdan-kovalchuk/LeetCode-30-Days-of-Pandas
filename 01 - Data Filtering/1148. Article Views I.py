4914493fb8ff438be1683d75855d545830ba425d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-07 20:37:40 +0300

    1148 solution

--- 01 - Data Filtering/1148. Article Views I.py
+++ 01 - Data Filtering/1148. Article Views I.py
@@ -1,1 +1,9 @@
- 
+import pandas as pd
+
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    mask = views["author_id"] == views["viewer_id"]
+    ids = views[mask][["author_id"]].rename(columns={"author_id": "id"})
+    unique = ids.drop_duplicates(subset=["id"])
+    sorted = unique.sort_values(by=["id"], ascending=True)
+    return sorted
