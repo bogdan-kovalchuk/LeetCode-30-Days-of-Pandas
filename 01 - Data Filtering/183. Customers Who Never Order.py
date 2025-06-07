043d2fb77aee672b59b3f1db5eae1b3d8ae02db8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-07 17:53:43 +0300

    183 solution

--- 01 - Data Filtering/183. Customers Who Never Order.py
+++ 01 - Data Filtering/183. Customers Who Never Order.py
@@ -1,1 +1,7 @@
- 
+import pandas as pd
+
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    mask = ~customers["id"].isin(orders["customerId"])
+    customers_no_orders = customers[mask][["name"]]
+    return customers_no_orders.rename(columns={"name": "Customers"})
