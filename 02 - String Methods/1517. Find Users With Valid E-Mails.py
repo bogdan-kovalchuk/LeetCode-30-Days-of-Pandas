92cd11b85abe080824b9ff562008aad9b49a4772
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-11 15:29:51 +0300

    1517 solution

--- 02 - String Methods/1517. Find Users With Valid E-Mails.py
+++ 02 - String Methods/1517. Find Users With Valid E-Mails.py
@@ -1,1 +1,10 @@
- 
+import pandas as pd
+import re
+
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    def is_valid_email(email: str) -> bool:
+        pattern = r"^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$"
+        return bool(re.fullmatch(pattern, email))
+
+    return users[users["mail"].apply(is_valid_email)]
