2bc8535c0ce769f5f6792540c404cbb587edfe65
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-12 16:00:19 +0300

    177 solution

--- 04 - Data Manipulation/177. Nth Highest Salary.py
+++ 04 - Data Manipulation/177. Nth Highest Salary.py
@@ -1,1 +1,11 @@
- 
+import pandas as pd
+import numpy as np
+
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    if N < 1:
+        return pd.DataFrame({f"getNthHighestSalary({N})": [np.nan]})
+
+    salaries = employee["salary"].drop_duplicates().nlargest(N)
+    nth_salary = salaries.iloc[-1] if len(salaries) == N else np.nan
+    return pd.DataFrame({f"getNthHighestSalary({N})": [nth_salary]})
