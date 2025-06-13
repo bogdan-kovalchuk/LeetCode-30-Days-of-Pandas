773f5f384a9b17bbe2fb2a027c8561c45c3a18c4
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-13 13:27:41 +0300

    176 solution

--- 04 - Data Manipulation/176. Second Highest Salary.py
+++ 04 - Data Manipulation/176. Second Highest Salary.py
@@ -1,1 +1,8 @@
- 
+import pandas as pd
+import numpy as np
+
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    salaries = employee["salary"].drop_duplicates().nlargest(2)
+    second = salaries.iloc[1] if len(salaries) == 2 else np.nan
+    return pd.DataFrame({"SecondHighestSalary": [second]})
