585fc8eac01a3bccf8991cf297e066e3e7d7e28b
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-13 14:50:56 +0300

    184 solution

--- 04 - Data Manipulation/184. Department Highest Salary.py
+++ 04 - Data Manipulation/184. Department Highest Salary.py
@@ -1,1 +1,10 @@
- 
+import pandas as pd
+
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    merged = employee.merge(department, left_on="departmentId", right_on="id")
+    merged["max_salary"] = merged.groupby("departmentId")["salary"].transform("max")
+    top_earners = merged[merged["salary"] == merged["max_salary"]]
+    return top_earners[["name_y", "name_x", "salary"]].rename(
+        columns={"name_y": "Department", "name_x": "Employee", "salary": "Salary"}
+    )
