fbf897bbdbb2fbe877b7ee2be96b4574cc491b40
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-15 19:15:17 +0300

    178 solution

--- 04 - Data Manipulation/178. Rank Scores.py
+++ 04 - Data Manipulation/178. Rank Scores.py
@@ -1,1 +1,13 @@
- 
+import pandas as pd
+
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    s = (
+        scores["score"]
+        .drop_duplicates()
+        .sort_values(ascending=False)
+        .to_frame(name="score")
+    )
+    s["rank"] = range(1, len(s) + 1)
+    d = pd.merge(scores, s)
+    return d[["score", "rank"]].sort_values(by="rank")
