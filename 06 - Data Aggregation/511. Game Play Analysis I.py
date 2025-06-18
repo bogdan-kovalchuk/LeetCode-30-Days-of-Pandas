bdefc3d09c3f49f792b178a23ad48feba9ddd0db
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-18 16:10:16 +0300

    511 solution

--- 06 - Data Aggregation/511. Game Play Analysis I.py
+++ 06 - Data Aggregation/511. Game Play Analysis I.py
@@ -1,1 +1,6 @@
- 
+import pandas as pd
+
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    activity = activity.rename(columns={"event_date": "first_login"})
+    return activity.groupby(by=["player_id"], as_index=False)["first_login"].min()
