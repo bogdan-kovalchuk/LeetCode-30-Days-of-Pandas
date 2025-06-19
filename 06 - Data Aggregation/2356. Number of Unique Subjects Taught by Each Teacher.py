45977d0a4f6b4f5f2a96fd0aa2654521f82a0fb9
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-19 12:16:41 +0300

    2356 solution

--- 06 - Data Aggregation/2356. Number of Unique Subjects Taught by Each Teacher.py
+++ 06 - Data Aggregation/2356. Number of Unique Subjects Taught by Each Teacher.py
@@ -1,1 +1,6 @@
- 
+import pandas as pd
+
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    d = teacher.groupby("teacher_id", as_index=False)["subject_id"].nunique()
+    return d.rename(columns={"subject_id": "cnt"})
