70ebf94dcea82d54c280fec31f2e23dde895ab2d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-22 08:10:50 +0300

    1484 solution

--- 06 - Data Aggregation/1484. Group Sold Products By The Date.py
+++ 06 - Data Aggregation/1484. Group Sold Products By The Date.py
@@ -1,1 +1,9 @@
- 
+import pandas as pd
+
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    g = activities.groupby("sell_date", as_index=False)
+    g = g["product"].agg(
+        [("num_sold", "nunique"), ("products", lambda x: ",".join(sorted(x.unique())))]
+    )
+    return g
