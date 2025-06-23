b7cf4f0aebc363fce647d402f0a3687eced09b50
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-23 11:40:31 +0300

    1050 solution

--- 07 - Data Integration/1050. Actors and Directors Who Cooperated At Least Three Times.py
+++ 07 - Data Integration/1050. Actors and Directors Who Cooperated At Least Three Times.py
@@ -1,1 +1,6 @@
- 
+import pandas as pd
+
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    g = actor_director.groupby(["actor_id", "director_id"], as_index=False).size()
+    return g[g["size"] >= 3][["actor_id", "director_id"]]
