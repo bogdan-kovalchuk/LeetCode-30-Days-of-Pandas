b8d446c565a79cdbec7d8d4ab824eccdd04af28a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-26 16:05:10 +0300

    570 solution

--- 07 - Data Integration/570. Managers with at Least 5 Direct Reports.py
+++ 07 - Data Integration/570. Managers with at Least 5 Direct Reports.py
@@ -1,1 +1,7 @@
- 
+import pandas as pd
+
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    managers_id_count = employee["managerId"].value_counts().reset_index()
+    filtered_managers_id = managers_id_count[managers_id_count["count"] >= 5]
+    return employee[employee["id"].isin(filtered_managers_id["managerId"])][["name"]]
