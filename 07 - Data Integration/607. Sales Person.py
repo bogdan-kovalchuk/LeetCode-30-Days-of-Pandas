6034283400996095732a0ddaec675d7b2e04a44b
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-27 20:12:46 +0300

    607 solution

--- 07 - Data Integration/607. Sales Person.py
+++ 07 - Data Integration/607. Sales Person.py
@@ -1,1 +1,9 @@
- 
+import pandas as pd
+
+
+def sales_person(
+    sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame
+) -> pd.DataFrame:
+    com_id = company[company["name"] == "RED"]["com_id"]
+    sales_ids = orders[orders["com_id"].isin(com_id)]["sales_id"]
+    return sales_person[~sales_person["sales_id"].isin(sales_ids)][["name"]]
