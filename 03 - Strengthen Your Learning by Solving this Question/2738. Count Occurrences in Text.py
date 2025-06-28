6c03fef1685dfa452a3d54531d5eff4397d11ce0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-28 19:07:11 +0300

    2738 solution

--- 03 - Strengthen Your Learning by Solving this Question/2738. Count Occurrences in Text.py
+++ 03 - Strengthen Your Learning by Solving this Question/2738. Count Occurrences in Text.py
@@ -1,1 +1,7 @@
- 
+import pandas as pd
+
+
+def count_occurrences(files: pd.DataFrame) -> pd.DataFrame:
+    bull_count = files["content"].str.contains(" bull ", case=False).sum()
+    bear_count = files["content"].str.contains(" bear ", case=False).sum()
+    return pd.DataFrame({"word": ["bull", "bear"], "count": [bull_count, bear_count]})
