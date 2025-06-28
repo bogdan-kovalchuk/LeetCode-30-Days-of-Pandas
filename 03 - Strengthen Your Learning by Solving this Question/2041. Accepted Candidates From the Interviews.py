b13842ff65ffae07aac07c95c5caea92d4a54e68
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-28 21:20:34 +0300

    2041 solution

--- 03 - Strengthen Your Learning by Solving this Question/2041. Accepted Candidates From the Interviews.py
+++ 03 - Strengthen Your Learning by Solving this Question/2041. Accepted Candidates From the Interviews.py
@@ -1,1 +1,7 @@
- 
+import pandas as pd
+
+
+def accepted_candidates(candidates: pd.DataFrame, rounds: pd.DataFrame) -> pd.DataFrame:
+    r = rounds.groupby("interview_id", as_index=False)["score"].sum()
+    c = pd.merge(candidates, r, how="left")
+    return c[(c["years_of_exp"] >= 3) & (c["score"] > 15)][["candidate_id"]]
