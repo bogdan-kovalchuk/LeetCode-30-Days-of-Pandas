6acd6e1c6872d08d8756710c04699fc6ae90316b
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-28 19:25:40 +0300

    1173 solution

--- 05 - Statistics/1173. Immediate Food Delivery I.py
+++ 05 - Statistics/1173. Immediate Food Delivery I.py
@@ -1,1 +1,9 @@
- 
+import pandas as pd
+
+
+def food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    immediate = (
+        delivery["order_date"] == delivery["customer_pref_delivery_date"]
+    ).sum()
+    total = delivery.shape[0]
+    return round(immediate / total * 100, 2)
